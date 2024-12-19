d8dc49c32c9ec15ba904b8ca35e8ab14fd486798
klluni <klluni33@gmail.com>
2024-12-19 18:14:34 +0300

    add slader

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+        speed: 1500,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev">Назад</button>',
+        nextArrow: '<button type="button" class="slick-next">Вперед</button>',
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
+    });
 });
