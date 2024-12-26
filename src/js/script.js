cb4ed00964e9ce01e812a90ae99306f491c2ce58
klluni <klluni33@gmail.com>
2024-12-27 00:44:19 +0300

    add modals windows

--- src/js/script.js
+++ src/js/script.js
@@ -17,9 +17,9 @@ $(document).ready(function() {
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass
-          ('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass
+            ('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
@@ -34,4 +34,20 @@ $(document).ready(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal windows
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay,  #consultation, #thanks, #order').fadeOut('slow')
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
